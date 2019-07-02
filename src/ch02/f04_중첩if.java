d1c4999450a74cf7b6e238b3517bd6f997a01784
Administrator <Administrator@FULLNAM-AORJU3G>
2019-07-02 10:46:01 +0900

    190702

---
+++ src/ch02/f04_중첩if.java
@@ -0,0 +1,63 @@
+package ch02;
+
+import java.util.Scanner;
+
+public class f04_중첩if {
+	/*
+	 * 	중첩문
+	 * 		제어문 안에 또 다른 제어문을 작성
+	 * 
+	 * 	중첩 if문
+	 * 		if문 안에 if문 작성
+	 * 		if(조건){
+	 * 			if(조건){
+	 * 			}
+	 * 		}
+	 * 
+	 * 정수 3개 입력 -> 2번째로 큰 정수 출력.
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = 10, b = 20, c = 5;
+		if(a > b) {
+			if(a > c)
+				System.out.println("a");
+			else
+				System.out.println("c");
+		}
+		else {
+			if(b > c)
+				System.out.println("b");
+			else
+				System.out.println("c");
+		}
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int z = sc.nextInt();
+		if(x > y) {
+			if(x > z) {
+				if(y > z)	//xyz
+					System.out.println("y " + y);
+				else	//xzy
+					System.out.println("z " + z);
+			}
+			else	//zxy
+				System.out.println("x " + x);
+		}
+		else {
+			if(y > z) {
+				if(x > z)	//yxz
+					System.out.println("x " + x);
+				else	//yzx
+					System.out.println("z " + z);
+			}
+			else	//zyx
+				System.out.println("y " + y);
+		}
+		
+		sc.close();
+	}
+
+}
